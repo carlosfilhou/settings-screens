51d0d65a04873c2843069467904453e695aa79b5
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-12-06 00:45:01 +0000

    Initial Commit

---
+++ SettingsScreens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SettingsScreens
+//
+//  Created by Carlos dos Santos Filho on 06/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SettingsScreens/SettingsScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsScreensApp.swift
+//  SettingsScreens
+//
+//  Created by Carlos dos Santos Filho on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SettingsScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
