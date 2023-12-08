5846a2af555ef9663a93c3649daa04c6c5c2a4cb
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-12-08 01:44:46 +0000

    feat slider-button

--- SettingsScreens/ContentView.swift
+++ SettingsScreens/ContentView.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var automaticButton: Bool = false
+    @State var toggleButton: Bool = true // o usuário pode alterar o estado dessa variável
+    @State var sliderButton: Double = 50.0 // o usuário pode alterar o estado dessa variável
+    
+    let brightnessTittle = "BRIGHTNESS"
+    let footerText = "When in Low Power Mode, auto-lock us restricted to 30 seconds."
     
     var body: some View {
         Form { // agrupador de elemento visual
@@ -11,7 +15,7 @@ struct ContentView: View {
                     Image(systemName: "airplane")
                     Text("Modo Avião")
                     Spacer()
-                    Toggle("",isOn:$automaticButton)
+                    Toggle("",isOn:$toggleButton) // fazendo o binding (source of truth)
                 }
                 HStack { // agrupador de elemento visual
                     Image(systemName: "wifi")
@@ -30,6 +34,17 @@ struct ContentView: View {
                 HStack { // agrupador de elemento visual
                     Image(systemName:"speaker.3.fill")
                     Text("Som e Tato")
+                }
+            }
+            Section(header: Text(brightnessTittle), footer: Text(footerText)) { // título no topo
+                HStack {
+                    Image(systemName: "sun.max.fill")
+                        .foregroundColor(.gray)
+                    Slider(value: $sliderButton, in: 0...100)
+                    Image(systemName: "sun.max.fill")
+                        .resizable() // modificador que possibilita mudar o tamanho
+                        .frame(width: 25, height: 25) // modificando o tamanho
+                        .foregroundColor(.gray)
                 }
             }
         }
